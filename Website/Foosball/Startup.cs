56de5ae0351ce24d81a96a8083a7904d487b8abd
Ferenc Hammerl <ferenchammerl@outlook.com>
2015-03-24 17:15:10 +0100

    Initial commit with VS project and docs

---
+++ Website/Foosball/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Foosball.Startup))]
+namespace Foosball
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
