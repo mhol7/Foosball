ccf7da8618981145a141341677d1b6b8e0e2d252
Ferenc Hammerl <ferenchammerl@outlook.com>
2015-04-15 17:44:43 +0200

    Match class is now functional with tests.

--- Website/Foosball/Models/FoosballClasses/Match.cs
+++ Website/Foosball/Models/FoosballClasses/Match.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.EnterpriseServices;
 using System.Linq;
 using Microsoft.Ajax.Utilities;
 
@@ -21,9 +22,39 @@ namespace Foosball.Models.FoosballClasses
         [Required]
         public bool[] Confirmations;
 
-        public bool IsConfirmed { get { return !Confirmations.Select(x => false).Any(); } }
+        public bool IsConfirmed { get { return !Confirmations.Where(x => !x).Any(); } } //is true if there is no x that is false
 
-        public void EloAlarm()
+        public Match() { }
+
+        public Match(string location, DateTime date, Player[] players, bool[] confirmations)
+        {
+            Location = location;
+            Date = date;
+            Players = players;
+            Confirmations = confirmations;
+        }
+
+        public Match(int matchId, string location, DateTime date, Player[] players, bool[] confirmations)
+        {
+            MatchId = matchId;
+            Location = location;
+            Date = date;
+            Players = players;
+            Confirmations = confirmations;
+        }
+
+        public bool Confirm(Player player)
+        {
+            int index = Players.ToList().IndexOf(Players.ToList().First(x => (x.UserId == player.UserId)));
+            if (index < 0) return false;
+            Confirmations[index] = true;
+            if (IsConfirmed)
+                EloAlarm();
+
+            return true;
+        }
+
+        void EloAlarm()
         {
             if (!IsConfirmed) return;
 
@@ -42,6 +73,7 @@ namespace Foosball.Models.FoosballClasses
                 Players[Players.Length - i - 1].CalculateEloLose(team1Elo);
             }
         }
+
     }
 
 }
